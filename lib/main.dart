85e4514b36b9e58922e4700baa65f9cdd53799ac
aluun-aae <ergesh228822@gmail.com>
2021-06-25 19:03:13 +0600

    sc

--- lib/main.dart
+++ lib/main.dart
@@ -36,87 +36,88 @@ class _MyHomePageState extends State<MyHomePage> {
   TextEditingController _controller = TextEditingController();
   String city = "Бишкек";
   var now = DateTime.now();
-
   @override
   Widget build(BuildContext context) {
+    print(now);
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(color: Colors.lime),
-        child: Column(
-          children: [
-            SizedBox(height: 30),
-            Center(
-              child: BlocBuilder(
-                bloc: bloc,
-                builder: (context, state) {
-                  if (state is WeatherInitial) {
-                    return Loading();
-                  } else if (state is WeatherLoaded) {
+      body: SingleChildScrollView(
+        child: Container(
+          height: height,
+          decoration: BoxDecoration(color: Colors.lime),
+          child: Column(
+            children: [
+              SizedBox(height: 30),
+              Center(
+                child: BlocBuilder(
+                  bloc: bloc,
+                  builder: (context, state) {
+                    if (state is WeatherInitial) {
+                      return Loading();
+                    } else if (state is WeatherLoaded) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          SizedBox(height: 30),
+                          Text(
+                            "${DateFormat('MMMEd').format(now)}",
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            "${DateFormat('jm').format(now)}",
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            state.model.cityName.toString(),
+                            style: TextStyle(fontSize: 50, color: Colors.white),
+                          ),
+                          Container(
+                            height: height / 3,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                        "https://openweathermap.org/img/wn/${state.model.iconCode}@4x.png"))),
+                          ),
+                          Text(
+                            state.model.temperature.toString() + " °",
+                            style: TextStyle(fontSize: 50, color: Colors.white),
+                          ),
+                        ],
+                      );
+                    } else if (state is WeatherError) {
+                      return Text("Oops connect failed");
+                    }
                     return Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
+                        Text(
+                          "",
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
                         SizedBox(height: 30),
-                        Text(
-                          "${DateFormat('MMMEd').format(now)}",
-                          style: TextStyle(fontSize: 20, color: Colors.white),
-                        ),
-                        SizedBox(
-                          height: 40,
-                        ),
-                        Text(
-                          "${DateFormat('hh:mm a').format(now)}",
-                          style: TextStyle(fontSize: 20, color: Colors.white),
-                        ),
-                        SizedBox(
-                          height: 40,
-                        ),
-                        Text(
-                          state.model.name.toString(),
-                          style: TextStyle(fontSize: 50, color: Colors.white),
-                        ),
-                        SizedBox(
-                          height: 40,
-                        ),
-                        Icon(
-                          Icons.wb_sunny_sharp,
-                          size: 200,
-                          color: Colors.amberAccent,
-                        ),
-                        SizedBox(
-                          height: 50,
-                        ),
-                        Text(
-                          state.model.main!.temp.toString() + " °",
-                          style: TextStyle(fontSize: 50, color: Colors.white),
-                        ),
                       ],
                     );
-                  } else if (state is WeatherError) {
-                    return Text("Oops connect failed");
-                  }
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "",
-                        style: Theme.of(context).textTheme.headline4,
-                      ),
-                      SizedBox(height: 30),
-                    ],
-                  );
-                },
+                  },
+                ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.all(20),
-              child: TextField(
-                controller: _controller,
-                decoration: InputDecoration(
-                    hintText: "Введите город",
-                    hintStyle: TextStyle(color: Colors.white)),
-              ),
-            )
-          ],
+              Container(
+                padding: EdgeInsets.all(20),
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                      hintText: "Введите город",
+                      hintStyle: TextStyle(color: Colors.white)),
+                ),
+              )
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
--- lib/model/weather_model.dart
+++ lib/model/weather_model.dart
@@ -1,81 +1,23 @@
 class WeatherModel {
-  Coord? coord;
-  String? base;
-  Main? main;
-  String? name;
-  int? cod;
-  int? timezone;
-  WeatherModel({
-    this.coord,
-    this.base,
-    this.main,
-    this.name,
-    this.cod,
-    this.timezone,
-  });
+  final String? cityName;
+  final int? temperature;
+  final String? iconCode;
+  final String? description;
+  final DateTime? time;
 
-  WeatherModel.fromJson(Map<String, dynamic> json) {
-    coord = json['coord'] != null ? new Coord.fromJson(json['coord']) : null;
-    base = json['base'];
-    main = json['main'] != null ? new Main.fromJson(json['main']) : null;
-    name = json['name'];
-    cod = json['cod'];
-    timezone = json["timezone"];
-  }
+  WeatherModel(
+      {this.cityName,
+      this.temperature,
+      this.iconCode,
+      this.description,
+      this.time});
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    if (this.coord != null) {
-      data['coord'] = this.coord!.toJson();
-    }
-    data['base'] = this.base;
-    if (this.main != null) {
-      data['main'] = this.main!.toJson();
-    }
-    data['timezone'] = this.timezone;
-    data['name'] = this.name;
-    data['cod'] = this.cod;
-    return data;
-  }
-}
-
-class Coord {
-  double? lon;
-  double? lat;
-
-  Coord({this.lon, this.lat});
-
-  Coord.fromJson(Map<String, dynamic> json) {
-    lon = json['lon'];
-    lat = json['lat'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['lon'] = this.lon;
-    data['lat'] = this.lat;
-    return data;
-  }
-}
-
-class Main {
-  double? temp;
-  double? feelsLike;
-  int? grndLevel;
-
-  Main({this.temp, this.feelsLike, this.grndLevel});
-
-  Main.fromJson(Map<String, dynamic> json) {
-    temp = json['temp'];
-    feelsLike = json['feels_like'];
-    grndLevel = json['grnd_level'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['temp'] = this.temp;
-    data['feels_like'] = this.feelsLike;
-    data['grnd_level'] = this.grndLevel;
-    return data;
+  factory WeatherModel.fromJson(Map<String, dynamic> json) {
+    return WeatherModel(
+        cityName: json['name'],
+        temperature: double.parse(json['main']['temp'].toString()).toInt(),
+        iconCode: json['weather'][0]['icon'],
+        description: json['weather'][0]['main'],
+        time: DateTime.fromMillisecondsSinceEpoch(json['dt'] * 1000));
   }
 }
